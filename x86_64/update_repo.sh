2a049622c998811b910dbeb36576fa125c988e86
amanre <amanre556@gmail.com>
2023-08-28 17:02:21 +0200

    update

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm nemesis_repo*
+rm custom_repo*
 
 echo "repo-add"
-repo-add -n -R nemesis_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R custom_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 
 echo "####################################"
